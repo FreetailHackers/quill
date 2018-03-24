db96e05f9bf60bde0fb6a3d79b1680e59ad115ac
Arjun Kunjilwar <arjunkunjilwar@utexas.edu>
2018-03-24 18:00:00 -0500

    Fixed template issue

--- app/server/services/email.js
+++ app/server/services/email.js
@@ -280,7 +280,7 @@ controller.sendConfirmationEmail = function(user, callback) {
    *   verifyUrl: the url that the user must visit to verify their account
    * }
    */
-  sendOne('email-link-action', options, locals, function(err, info){
+  sendOne('email-confirmation', options, locals, function(err, info){
     if (err){
       console.log(err);
     }
